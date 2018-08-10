adde8fb5f46d665332bacf80873aa4945146fe94
Moises Ramirez <source.moises@gmail.com>
2018-08-10 10:40:34 -0400

    complete meal buttons

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,11 +14,41 @@ export class HomePage {
   }
 
   private milkQuantity = 0;
+  private breadQuantity = 0;
+  private oilQuantity = 0;
+  private fruitQuantity = 0;
+  private vegetableQuantity = 0;
+  private proteinQuantity = 0;
+
   private incrementMilk(){
     this.milkQuantity++;
   }
 
+  private incrementBread(){
+    this.breadQuantity++;
+  }
+
+  private incrementOil(){
+    this.oilQuantity++;
+  }
+
+  private incrementFruit(){
+    this.fruitQuantity++;
+  }
+
+  private incrementVegetable(){
+    this.vegetableQuantity++;
+  }
+
+  private incrementProtein(){
+    this.proteinQuantity++;
+  }
   private clearAllMeal(){
     this.milkQuantity = 0;
+    this.breadQuantity = 0;
+    this.oilQuantity = 0;
+    this.fruitQuantity = 0;
+    this.vegetableQuantity = 0;
+    this.proteinQuantity = 0;
   }
 }
