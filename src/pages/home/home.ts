148565003a6ce4c26c040efa9efef4189aa5c5f6
Moises Ramirez <source.moises@gmail.com>
2018-08-13 17:26:16 -0300

    adds search bar and add nutrition facts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,7 @@ import { Storage } from '@ionic/storage';
 export class HomePage {
 
   private milkQuantity = 0;
-  private breadQuantity = 0;
+  private carbQuantity = 0;
   private oilQuantity = 0;
   private fruitQuantity = 0;
   private vegetableQuantity = 0;
@@ -22,8 +22,8 @@ export class HomePage {
       this.milkQuantity = val || 0;
     });
 
-    storage.get('bread').then((val) => {
-      this.breadQuantity = val || 0;
+    storage.get('carb').then((val) => {
+      this.carbQuantity = val || 0;
     });
 
     storage.get('oil').then((val) => {
@@ -43,40 +43,40 @@ export class HomePage {
     });
   }
 
-  private incrementMilk(){
-    this.milkQuantity++;
+  private incrementMilk(quantity = 1){
+    this.milkQuantity += quantity;
     this.storage.set('milk', this.milkQuantity);
   }
 
-  private incrementBread(){
-    this.breadQuantity++;
-    this.storage.set('bread', this.breadQuantity);
+  private incrementCarb(quantity = 1){
+    this.carbQuantity += quantity;
+    this.storage.set('carb', this.carbQuantity);
   }
 
-  private incrementOil(){
-    this.oilQuantity++;
+  private incrementOil(quantity = 1){
+    this.oilQuantity += quantity;
     this.storage.set('oil', this.oilQuantity);
   }
 
-  private incrementFruit(){
-    this.fruitQuantity++;
+  private incrementFruit(quantity = 1){
+    this.fruitQuantity += quantity;
     this.storage.set('fruit', this.fruitQuantity);
   }
 
-  private incrementVegetable(){
-    this.vegetableQuantity++;
+  private incrementVegetable(quantity = 1){
+    this.vegetableQuantity += quantity;
     this.storage.set('vegetable', this.vegetableQuantity);
   }
 
-  private incrementProtein(){
-    this.proteinQuantity++;
+  private incrementProtein(quantity = 1){
+    this.proteinQuantity += quantity;
     this.storage.set('protein', this.proteinQuantity);
   }
   private clearAllMeal(){
     this.storage.set('milk', 0);
     this.milkQuantity = 0;
-    this.storage.set('bread', 0);
-    this.breadQuantity = 0;
+    this.storage.set('carb', 0);
+    this.carbQuantity = 0;
     this.storage.set('oil', 0);
     this.oilQuantity = 0;
     this.storage.set('fruit', 0);
@@ -85,5 +85,61 @@ export class HomePage {
     this.vegetableQuantity = 0;
     this.storage.set('protein', 0);
     this.proteinQuantity = 0;
+  }
+
+  public addQuantity(meal){
+    if(meal.nutritionFacts[0].type === 'protein'){
+      this.incrementProtein();
+    }else if (meal.nutritionFacts[0].type === 'carb'){
+      this.incrementCarb();
+    }else if (meal.nutritionFacts[0].type === 'milk'){
+      this.incrementMilk();
+    }else if (meal.nutritionFacts[0].type === 'vegetable'){
+      this.incrementVegetable();
+    }else if (meal.nutritionFacts[0].type === 'fruit'){
+      this.incrementFruit();
+    }else if (meal.nutritionFacts[0].type === 'oil'){
+      this.incrementOil();
+    }
+    meal.name = '';
+    meal.quantity = '';
+  }
+
+  private mealList:Array<any>;
+
+  private initializeItems(){
+    this.mealList = [
+      {"name":"Pollo", "quantity":"200 gr", "nutritionFacts" : [{"type":"protein"}]},
+      {"name":"Avena", "quantity":"1/4 taza", "nutritionFacts" : [{"type":"carb"}]},
+      {"name":"Quesillo", "quantity":"3 rebanadas", "nutritionFacts" : [{"type":"milk"}]},
+      {"name":"Zanahoria", "quantity":"1/3 taza", "nutritionFacts" : [{"type":"vegetable"}]},
+      {"name":"Pasas", "quantity":"35 unidades", "nutritionFacts" : [{"type":"fruit"}]},
+      {"name":"Mani", "quantity":"1 puñado", "nutritionFacts" : [{"type":"oil"}]}
+    ]
+  }
+
+  public onCancel(searchbar){
+    this.mealList = [];
+    console.log(">>>>>> Cancel");
+  }
+
+  public getItems(searchbar) {
+    this.initializeItems();
+    var q = searchbar.srcElement.value;
+    console.log(">>>>>> getItems " + q);
+
+    if (!q){ 
+      this.mealList = [];
+      return false;
+    }
+  
+    this.mealList = this.mealList.filter((v) => {
+      if(v.name && q) {
+        if (v.name.toLowerCase().indexOf(q.toLowerCase()) > -1) {
+          return true;
+        }
+        return false;
+      }
+    });  
   }
 }
